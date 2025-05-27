fe4a2c7ffcd6b9a9032ae5856916d1ed62f6fde9
moiz2405 <km856307@gmail.com>
2025-05-27 17:27:09 +0000

    Update: credit/tasks.py

--- credit/tasks.py
+++ credit/tasks.py
@@ -6,33 +6,45 @@ from datetime import datetime
 @shared_task
 def ingest_customer_data():
     df = pd.read_excel("customer_data.xlsx")
+    # Normalize columns: lowercase and strip spaces
+    df.columns = df.columns.str.strip().str.lower()
+    print(df.columns.tolist())
+
     for _, row in df.iterrows():
-        Customer.objects.get_or_create(
-            customer_id=row['customer_id'],
-            first_name=row['first_name'],
-            last_name=row['last_name'],
-            phone_number=row['phone_number'],
-            monthly_salary=row['monthly_salary'],
-            approved_limit=row['approved_limit'],
-            current_debt=row['current_debt']
+        Customer.objects.update_or_create(
+            customer_id=row['customer id'],
+            defaults={
+                'first_name': row['first name'],
+                'last_name': row['last name'],
+                'phone_number': row['phone number'],
+                'monthly_salary': row['monthly salary'],
+                'approved_limit': row['approved limit'],
+                'current_debt': 0
+            }
         )
 
 @shared_task
 def ingest_loan_data():
     df = pd.read_excel("loan_data.xlsx")
+    df.columns = df.columns.str.strip().str.lower()
+    print(df.columns.tolist())
+
     for _, row in df.iterrows():
         try:
             customer = Customer.objects.get(customer_id=row['customer id'])
         except Customer.DoesNotExist:
             continue
-        Loan.objects.create(
+
+        Loan.objects.update_or_create(
             loan_id=row['loan id'],
-            customer=customer,
-            loan_amount=row['loan amount'],
-            tenure=row['tenure'],
-            interest_rate=row['interest rate'],
-            monthly_installment=row['monthly repayment (emi)'],
-            emis_paid_on_time=row['EMIs paid on time'],
-            start_date=pd.to_datetime(row['start date']),
-            end_date=pd.to_datetime(row['end date']),
+            defaults={
+                'customer': customer,
+                'loan_amount': row['loan amount'],
+                'tenure': row['tenure'],
+                'interest_rate': row['interest rate'],
+                'monthly_installment': row['monthly payment'],
+                'emis_paid_on_time': row['emis paid on time'],
+                'start_date': pd.to_datetime(row['date of approval']).date(),
+                'end_date': pd.to_datetime(row['end date']).date(),
+            }
         )
